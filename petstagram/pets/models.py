8db0d913b33a96ed58bafadbe545d79fb4e0a1da
danisp24 <danispasov24@gmail.com>
2024-10-14 20:14:04 +0300

    fixed pet model

--- petstagram/pets/models.py
+++ petstagram/pets/models.py
@@ -14,14 +14,18 @@ class Pet(models.Model):
     )
 
     slug = models.SlugField(
-        null=False,
+        null=True,
         blank=True,
         unique=True,
         editable=False
     )
 
     def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
         if not self.slug:
             self.slug = slugify(f'{self.name}-{self.id}')
 
         super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
